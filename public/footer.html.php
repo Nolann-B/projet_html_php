71ab7b66ad578798b5168e582f0cf074f9480132
Machamp <nolann.bourimech@lepoles.org>
2024-05-27 16:59:14 +0200

    27_05

---
+++ public/footer.html.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="footer">
+        <img src="" alt="fleche_bleue">
+    </div>
+</footer>
---
+++ public/nav.html.php
@@ -0,0 +1,90 @@
+<nav>
+    <div id="nav">
+        <ul>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Structure
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Texte
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Selecteur
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Image
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Position
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Zoning
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Tableau
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Liste
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Lien & Menu
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Formulaire
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Multimedia
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="liens">
+                    <a href="">
+                        Animation
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
