c5687aba389e2d003d357ff7361c8579feacd89a
Machamp <nolann.bourimech@lepoles.org>
2024-06-19 16:59:17 +0200

    19_06
    index

--- public/footer.html.php
+++ public/footer.html.php
@@ -1,8 +1,8 @@
 <footer>
     <div id="footer">
         &copy; 2020 Le Poles
-        <img src="" alt="fleche_bleue">
     </div>
+    <div id="footer_img"><i class="fa-solid fa-circle-arrow-up fa-2xl"></i></div>
 </footer>
 </div>
 </body>
--- public/header.html.php
+++ public/header.html.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="public/assets/css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Petit site</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
     <div id="body">
     <header>
         <div id="header">
             <div id="header1">
-                <img src="public/assets/img/logo.png" alt="logo css">
+                <a href="../projet_html_php/index.php" target=_blank><img src="public/assets/img/logo.png" alt="logo css"></a>
                 <!-- <img src="" alt="logo html"> -->
             </div>
             <div id="header2">
